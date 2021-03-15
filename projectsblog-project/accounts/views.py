479455acb67775c10deeb41cc77ab002e01f232a
99012 <62183376+99012@users.noreply.github.com>
2021-03-15 07:44:38 +0100

    signup created

---
+++ projectsblog-project/accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        # user wants to sign up - POST request
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # check if user with username already exists
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'User name has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords must match'})
+    else:
+        # user wants to enter info - GET request
+        return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # TODO need to route to homepage & logout
+    return render(request, 'accounts/signup.html')
